2dab8c5b75e7d205e22330444cf5c56de555ec12
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-14 16:55:16 +0300

    rotation fixed

--- app/src/main/java/com/najed/headsupv2/CelebActivity.kt
+++ app/src/main/java/com/najed/headsupv2/CelebActivity.kt
@@ -3,8 +3,13 @@ package com.najed.headsupv2
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import io.paperdb.Paper
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,33 +19,40 @@ class CelebActivity : AppCompatActivity() {
 
     var celebCount = 0
     lateinit var celebs: Celeb
+    lateinit var landscapeLayout: LinearLayout
+    lateinit var portraitLayout: ConstraintLayout
     lateinit var celebTextViews: List<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_celeb)
-        celebTextViews = listOf(
-            findViewById(R.id.celeb_name_tv),
-            findViewById(R.id.celeb_taboo1_tv),
-            findViewById(R.id.celeb_taboo2_tv),
-            findViewById(R.id.celeb_taboo3_tv)
-        )
+        landscapeLayout = findViewById(R.id.celeb_info_layout)
+        portraitLayout = findViewById(R.id.portrait_layout)
+
         Paper.init(this)
         celebs = Paper.book().read("celebs", Celeb())
-        if (celebs.isEmpty())
+        if (celebs.isEmpty()){
             setCelebs()
-        else {
-            celebCount++
+            Log.d("celebCount", "inside if celebs.isEmpty()")
         }
-        updateCelebViews()
+        else {
+            updateCelebViews()
+            Log.d("celebCount", "inside else, the count is $celebCount")
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
-            setContentView(R.layout.activity_celeb)
-        else
-            setContentView(R.layout.portrait_layout)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            updateCelebViews()
+            portraitLayout.visibility = INVISIBLE
+            landscapeLayout.visibility = VISIBLE
+        }
+        else {
+            celebCount++
+            landscapeLayout.visibility = INVISIBLE
+            portraitLayout.visibility = VISIBLE
+        }
     }
 
     private fun setCelebs() {
@@ -50,6 +62,7 @@ class CelebActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Celeb?>, response: Response<Celeb?>) {
                 celebs = response.body()!!
                 Paper.book().write("celebs", celebs)
+                updateCelebViews()
             }
             override fun onFailure(call: Call<Celeb?>, t: Throwable) {
                 Toast.makeText(this@CelebActivity, t.message, Toast.LENGTH_SHORT).show()
@@ -59,6 +72,12 @@ class CelebActivity : AppCompatActivity() {
     }
 
     private fun updateCelebViews(){
+        celebTextViews = listOf (
+            findViewById(R.id.celeb_name_tv),
+            findViewById(R.id.celeb_taboo1_tv),
+            findViewById(R.id.celeb_taboo2_tv),
+            findViewById(R.id.celeb_taboo3_tv)
+        )
         val currentCeleb = celebs[celebCount]
         celebTextViews[0].text = currentCeleb.name
         celebTextViews[1].text = currentCeleb.taboo1
