71520b1299a8e446fc7bc59d01752dc56eabe768
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-14 17:20:28 +0300

    added some comment cuz I feel that I need them later

--- app/src/main/java/com/najed/headsupv2/CelebActivity.kt
+++ app/src/main/java/com/najed/headsupv2/CelebActivity.kt
@@ -17,11 +17,11 @@ import retrofit2.Response
 
 class CelebActivity : AppCompatActivity() {
 
-    var celebCount = 0
-    lateinit var celebs: Celeb
-    lateinit var landscapeLayout: LinearLayout
-    lateinit var portraitLayout: ConstraintLayout
-    lateinit var celebTextViews: List<TextView>
+    var celebCount = 0 // to iterate celebrities
+    lateinit var celebs: Celeb // the list of celebrities
+    lateinit var landscapeLayout: LinearLayout // the layout displayed in landscape mode
+    lateinit var portraitLayout: ConstraintLayout // the layout displayed in portrait mode
+    lateinit var celebTextViews: List<TextView> // the list of celebrity TextViews (name and taboos)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +29,21 @@ class CelebActivity : AppCompatActivity() {
         landscapeLayout = findViewById(R.id.celeb_info_layout)
         portraitLayout = findViewById(R.id.portrait_layout)
 
-        Paper.init(this)
-        celebs = Paper.book().read("celebs", Celeb())
-        if (celebs.isEmpty()){
-            setCelebs()
+        /* Paper is a library alternative to shared preferences
+           but works as a fast NoSQL storage. Helped me in storing
+           data of non-primitive type, in this case is Celeb which
+           is an array list of CelebItems.
+           I chose to store data so I don't bother calling the API
+           each time running the app.
+         */
+        Paper.init(this) // initialize paper object
+        celebs = Paper.book().read("celebs", Celeb()) // get stored Celeb object. If there's none, the default value is an empty object
+
+        if (celebs.isEmpty()){ // first time running the app, or data's wiped
+            setCelebs() // get data from the API
             Log.d("celebCount", "inside if celebs.isEmpty()")
         }
-        else {
+        else { // not the first time running the app but at the beginning of the game
             updateCelebViews()
             Log.d("celebCount", "inside else, the count is $celebCount")
         }
@@ -44,12 +52,12 @@ class CelebActivity : AppCompatActivity() {
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            updateCelebViews()
+            updateCelebViews() // the counter increased when phone was in portrait mode so we just have to update the views
             portraitLayout.visibility = INVISIBLE
             landscapeLayout.visibility = VISIBLE
         }
         else {
-            celebCount++
+            celebCount++ // to get new celebrity when phone get back to landscape mode
             landscapeLayout.visibility = INVISIBLE
             portraitLayout.visibility = VISIBLE
         }
@@ -60,9 +68,9 @@ class CelebActivity : AppCompatActivity() {
         val call: Call<Celeb?>? = apiInterface!!.getCelebs()
         call?.enqueue(object: Callback<Celeb?> {
             override fun onResponse(call: Call<Celeb?>, response: Response<Celeb?>) {
-                celebs = response.body()!!
-                Paper.book().write("celebs", celebs)
-                updateCelebViews()
+                celebs = response.body()!! // set the response to the object to be iterated through
+                updateCelebViews() // to be displayed first time
+                Paper.book().write("celebs", celebs) // store it in paper
             }
             override fun onFailure(call: Call<Celeb?>, t: Throwable) {
                 Toast.makeText(this@CelebActivity, t.message, Toast.LENGTH_SHORT).show()
@@ -71,7 +79,8 @@ class CelebActivity : AppCompatActivity() {
         })
     }
 
-    private fun updateCelebViews(){
+    // responsible of finding & updating celebrity TextViews
+    private fun updateCelebViews() {
         celebTextViews = listOf (
             findViewById(R.id.celeb_name_tv),
             findViewById(R.id.celeb_taboo1_tv),
